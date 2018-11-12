bcc9d3089f24dcc0e7c2395564772466f54d3307
zrshishir <zrshishir@gmail.com>
2018-11-12 18:27:48 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Styles -->
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height" id="app">
+            
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
